372268d539ba98109914f39a4e1e1f52f22d41c6
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-04-02 00:12:02 +0000

    fix: Removing unused categories endpoints

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('categories', 'Api\CategoryController');
+Route::group(['namespace' => 'Api'], function () {
+    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+});
