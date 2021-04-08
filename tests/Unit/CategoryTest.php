d37fd193f32deeb40bde8b74235fe761e8f30de0
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-04-07 23:20:47 -0300

    test: Implementing Category unit tests

---
+++ tests/Unit/CategoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+    public function testFillableAttribute()
+    {
+        $fillable = ['name', 'description', 'is_active'];
+        $category = new Category();
+        $this->assertEquals(
+            $fillable,
+            $category->getFillable()
+        );
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class, Uuid::class
+        ];
+        $categoryTraits = array_keys(class_uses(Category::class));
+        $this->assertEquals($traits, $categoryTraits);
+    }
+
+    public function testIncrementing()
+    {
+        $category = new Category();
+        $this->assertFalse($category->incrementing);
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $category = new Category();
+        foreach ($dates as $date) {
+            $this->assertContains($date, $category->getDates());
+        }
+        $this->assertCount(count($dates), $category->getDates());
+    }
+}
