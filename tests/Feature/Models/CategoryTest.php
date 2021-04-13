8f1dbd70e84232c33fdabda63a1cbf916b535708
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-04-13 09:21:17 -0300

    refactor: removing obsolete imports

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature\Models;
 
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
